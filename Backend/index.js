0e62af1211bf62a8b60e924298812eb6012a91d3
Nawab Kalra <nawabkalra@gmail.com>
2025-08-14 11:32:42 +0530

    Post ans put request

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const { createTodo, updateTodo } = require("./types")
 const app = express()
 
 app.use(express.json)
@@ -8,11 +9,26 @@ app.get('/todos', (req, res) => {
 })
 
 app.post('/todo', (req, res) => {
-    
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+        return;
+    }
 })
 
 app.put('/completed', (req, res) => {
+    const createPayload = req.body;
+    const parsedPayload = updateTodo.safeParse(createPayload);
     
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg:"You sent the wrong inputs"
+        })
+        return;
+    }
 })
 
 app.delete('/', (req, res) => {
--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 This project contains a simple TODO application.
 It has the following feature-
 
--Anyone can create a todos.
--Anyone can see their existing todos.
--Anyone can mark a todos as done.
+-Anyone can create a todos.  
+-Anyone can see their existing todos.  
+-Anyone can mark a todos as done.  
