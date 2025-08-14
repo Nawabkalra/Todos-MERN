16d16d31b6b16f1e8c06da569c5e17cdbe9e7249
NawabKalra <nawabkalra@gmail.com>
2025-08-14 12:28:35 +0530

    mongoose

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,18 @@
 const express = require("express")
 const { createTodo, updateTodo } = require("./types")
+const { todo } = require("./db")
 const app = express()
 
 app.use(express.json)
 
-app.get('/todos', (req, res) => {
-    
+app.get('/todos',async (req, res) => {
+    const todos = await todo.find({})
+    res.json({
+        todos
+    })
 })
 
-app.post('/todo', (req, res) => {
+app.post('/todo',async (req, res) => {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if(!parsedPayload.success) {
@@ -17,9 +21,19 @@ app.post('/todo', (req, res) => {
         })
         return;
     }
+    
+    await todo.create({
+        title : createPayload.title,
+        description : createPayload.description,
+        completed: false
+    })
+
+    res.json({
+        msg: "Todo created"
+    })
 })
 
-app.put('/completed', (req, res) => {
+app.put('/completed',async (req, res) => {
     const createPayload = req.body;
     const parsedPayload = updateTodo.safeParse(createPayload);
     
@@ -29,6 +43,15 @@ app.put('/completed', (req, res) => {
         })
         return;
     }
+
+    await todo.update({
+        _id: req.body.id
+    }, {
+        completed: true
+    })
+    res.json({
+        msg: "todo marked as completed"
+    })
 })
 
 app.delete('/', (req, res) => {
